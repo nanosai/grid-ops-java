7a3573bb34556cd436b5a6f214908e8b78451422
Jakob Jenkov <jakob@jenkov.com>
2016-08-20 16:50:51 +0200

    First import of IAP Tools ION APIs into Grid Ops

---
+++ src/main/java/com/nanosai/gridops/ion/read/IIonFieldReader.java
@@ -0,0 +1,13 @@
+package com.nanosai.gridops.ion.read;
+
+/**
+ * Created by jjenkov on 05-11-2015.
+ */
+public interface IIonFieldReader {
+
+    public int read(byte[] source, int sourceOffset, Object destination);
+
+    public void setNull(Object destination);
+
+
+}
