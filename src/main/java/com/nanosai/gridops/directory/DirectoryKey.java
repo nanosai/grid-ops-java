9e601d74dd7b831415594978103775ec3a4cbf8c
Jakob Jenkov <jakob@jenkov.com>
2016-11-05 18:03:06 +0100

    Equals updated

--- src/main/java/com/nanosai/gridops/directory/DirectoryKey.java
+++ src/main/java/com/nanosai/gridops/directory/DirectoryKey.java
@@ -7,35 +7,68 @@ import java.util.Arrays;
  */
 public class DirectoryKey {
 	
-	private byte[] id;
+	private byte[] idSource;
+	private int    idOffset;
+	private int    idLength;
 	
 	@Override
 	public boolean equals (Object o) {
-		
-		if(o instanceof DirectoryKey){
-			DirectoryKey directoryKey = (DirectoryKey) o;
-			
-			if(Arrays.equals(directoryKey.getId(), this.getId())){
-				return true;
-			}else{
-				return false;
-			}
-		}else{
+
+		if( !(o instanceof DirectoryKey)){
 			return false;
 		}
+		
+		DirectoryKey otherDirectoryKey = (DirectoryKey) o;
+
+        if(this.idLength != otherDirectoryKey.idLength){
+            return false;
+        }
+        for(int i=0; i<this.idLength; i++){
+            if(this.idSource[this.idOffset + i] != otherDirectoryKey.idSource[otherDirectoryKey.idOffset + i]){
+                return false;
+            }
+        }
+
+        return true;
+
 	}
 	
 	@Override
 	public int hashCode() {
 		
-		return Arrays.hashCode(this.getId());
-	 }
-
-	public byte[] getId() {
-		return id;
+		return Arrays.hashCode(this.getIdSource());
 	}
 
-	public void setId(byte[] id) {
-		this.id = id;
+    public DirectoryKey() {
+    }
+
+    public DirectoryKey(byte[] idSource, int idOffset, int idLength) {
+		this.idSource = idSource;
+		this.idOffset = idOffset;
+		this.idLength = idLength;
+	}
+
+	public byte[] getIdSource() {
+		return idSource;
+	}
+
+	public void setIdSource(byte[] idSource) {
+		this.idSource = idSource;
+	}
+
+	public int getIdOffset() {
+		return idOffset;
+	}
+
+	public void setIdOffset(int idOffset) {
+		this.idOffset = idOffset;
+	}
+
+	public int getIdLength() {
+		return idLength;
+	}
+
+	public void setIdLength(int idLength) {
+		this.idLength = idLength;
 	}
 }
