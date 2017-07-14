49e7d8d4a3a39b993b9517852b90f447e9318f02
Jakob Jenkov <jakob@jenkov.com>
2017-07-14 11:46:50 +0200

    JavaDoc updated

--- src/main/java/com/nanosai/gridops/ion/codec/IonCodec.java
+++ src/main/java/com/nanosai/gridops/ion/codec/IonCodec.java
@@ -4,7 +4,9 @@ import com.nanosai.gridops.ion.read.IonReader;
 import com.nanosai.gridops.ion.write.IonWriter;
 
 /**
- * Created by jjenkov on 26-11-2016.
+ * An IonCodec implementation is capable of reading and writing some data structure to and from the ION data format.
+ * Such implementations are often simple value objects representing full or partial IAP messages.
+ *
  */
 public interface IonCodec {
 
